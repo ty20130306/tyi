218bd1927bf163a39b5abb864b59011c37468b7c
wolf <tongyou2013@163.com>
2013-03-16 11:40:55 +0800

    add sample

---
+++ sample.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'tyi.php';
+require_once 'Smarty.class.php';
+
+// step 1. configure your output type of html
+$htmlOutputCfg = array(
+	'template_dir'	=> your/dir/to/template,
+	'compile_dir'	=> your/dir/to/compile
+);
+TysOutput::initTypeCfg(TysOutput::TYPE_HTML, $htmlOutputCfg);
+
+// step 2. configure your log dir 
+Tyi_Helper_Log::init(your/dir/to/log/runtime, your/dir/to/log/action);
+
+// step 3. run it
+Tyi::run(your/dir/to/tys);
