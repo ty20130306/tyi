70d42a3877fde7c543dbe0578d6f4d956dc689ee
wolf <tongyou2013@163.com>
2013-03-18 18:29:28 +0800

    simplify usage: update sample

--- sample.php
+++ sample.php
@@ -3,15 +3,11 @@
 require_once 'tyi.php';
 require_once 'Smarty.class.php';
 
-// step 1. configure your output type of html
-$htmlOutputCfg = array(
-	'template_dir'	=> your/dir/to/template,
-	'compile_dir'	=> your/dir/to/compile
-);
-TysOutput::initTypeCfg(TysOutput::TYPE_HTML, $htmlOutputCfg);
+// step 1. configure your smarty
+Tyi::initSmartyCfg(your/dir/to/template, your/dir/to/compile);
 
 // step 2. configure your log dir 
-TyiLog::init(your/dir/to/log/runtime, your/dir/to/log/action);
+Tyi::initLog(your/dir/to/log/runtime, your/dir/to/log/action);
 
 // step 3. run it
 Tyi::run(your/dir/to/tys);
